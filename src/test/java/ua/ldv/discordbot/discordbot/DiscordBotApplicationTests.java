a45e19b764c3fe566766c46c45ba9fb2dcecec6e
nugos <ldv@ldv.com.ua>
2023-12-24 12:58:06 +0200

    git init

---
+++ src/test/java/ua/ldv/discordbot/discordbot/DiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ldv.discordbot.discordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
